680558b5504fb9d39865db4d970e278660eac177
Evan Furniss <evanfurniss@gmail.com>
2020-12-27 15:04:29 -0600

    Edited log function to print values of number of dice, size of dice, adding the modifier, and total value.

--- script.js
+++ script.js
@@ -5,8 +5,17 @@
 // console.log random number based on diceSize and diceNum
     // for loop?    (let i=0; i<_____; i++)
 // console.log random number based on all 3 inputs
+
 document.getElementById("rollButton").addEventListener("click", rollFunction);
 
 function rollFunction() {
-  console.log("Hello World!")
+  let diceNum = parseInt(document.getElementById("diceNum").value);
+  let diceSize = parseInt(document.getElementById("diceSize").value);
+  let modifier = parseInt(document.getElementById("modifier").value);
+
+  console.log("Number of dice: " + diceNum);
+  console.log("Size of dice: " + diceSize);
+  console.log("Dice modifier: " + modifier);
+
+  console.log("Total: " + (diceNum * diceSize + modifier));
 }
