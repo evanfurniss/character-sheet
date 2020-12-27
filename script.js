a2ff8e830abbe864fb2ff37ec63a85b833b4f68c
Evan Furniss <evanfurniss@gmail.com>
2020-12-27 12:26:29 -0600

    added initial dice roll framework.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Console.log("Hello World!") on button click
+// console.log each of the 3 inputs on button click.
+// console.log random number based on diceSize
+    // math.floor(math.random()*diceSize)
+// console.log random number based on diceSize and diceNum
+    // for loop?    (let i=0; i<_____; i++)
+// console.log random number based on all 3 inputs
